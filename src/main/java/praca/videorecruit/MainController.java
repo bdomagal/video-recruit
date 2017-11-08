de3d7a75103dac56a56770dd0a78043193b53fd9
Bartek <bartosz.domagala1@gmail.com>
2017-11-09 00:25:19 +0100

    Start

---
+++ src/main/java/praca/videorecruit/MainController.java
@@ -0,0 +1,13 @@
+package praca.videorecruit;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+
+    @GetMapping("/")
+    public String hello(){
+        return "hello";
+    }
+}
---
+++ src/test/java/praca/videorecruit/VideorecruitApplicationTests.java
@@ -0,0 +1,16 @@
+package praca.videorecruit;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class VideorecruitApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
