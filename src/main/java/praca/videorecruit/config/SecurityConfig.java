4fd9113eef4d9f1f8065102b2cbd30a39eea602e
Bartosz Domagala <bartosz.domagala1@gmail.com>
2018-01-21 21:17:35 +0100

    Merged

--- src/main/java/praca/videorecruit/config/SecurityConfig.java
+++ src/main/java/praca/videorecruit/config/SecurityConfig.java
@@ -1,15 +1,12 @@
 package praca.videorecruit.config;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationProvider;
-import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
@@ -22,13 +19,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 
 
 
-    final
-    UserDetailsService userDetailsService;
 
-    @Autowired
-    public SecurityConfig(UserDetailsService userDetailsService) {
-        this.userDetailsService = userDetailsService;
-    }
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
@@ -44,6 +35,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
                     .antMatchers("/login**", "/register**", "/registerUser", "/registerComp").anonymous()
                     .antMatchers("/createOffer", "myProfile/company**").hasAnyRole("COMPANY", "ADMIN")
                     .antMatchers("/offers", "myProfile/person**").hasAnyRole("USER", "ADMIN")
+                    .antMatchers("/").permitAll()
                     .anyRequest().authenticated()
                     .and()
                 .formLogin()
